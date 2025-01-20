be4cdf3a12e84a9ec9c2f752ea3e9321ae55a642
Mohammad Shoaib Elias <mishoel344@gmail.com>
2025-01-21 04:20:09 +0600

    Update readme

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: FC = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 2000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
